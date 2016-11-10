b53cad03232295489df227fec0362381193c2dbf
Belyaev Anton Yurevich <fxdev-belyaev-ay@fxclub.org>
2016-11-10 16:39:44 +0700

    https://github.com/avvero/conversation_analyzer_view.git

--- src/main/resources/static/js/controllers/conversationController.js
+++ src/main/resources/static/js/controllers/conversationController.js
@@ -1,73 +1,82 @@
 function conversationController($scope, data, $stateParams) {
+    $scope.emotionTones = ["Anger", "Disgust", "Fear", "Joy", "Sadness"]
+    $scope.emotionToneId = 0
+    $scope.setEmotionToneId = function(id) {
+        $scope.emotionToneId = id
+        $scope.updateData()
+    }
     $scope.series = []
     $scope.data = []
-    $scope.labels = [];
+    $scope.labels = []
+    $scope.options = {}
 
-    var groupedData = {}
-    var accounts = {}
-    for (var i = 0; i < data.length; i++) {
-        if (!accounts[data[i].from.id]) {
-            var name = data[i].from.name || data[i].from.id
-            accounts[data[i].from.id] = name
-        } else {
-            if(accounts[data[i].from.id] == data[i].from.id && data[i].from.name) {
-                accounts[data[i].from.id] = data[i].from.name
-            }
-        }
-    }
-    for (var account in accounts) {
-        $scope.series.push(accounts[account])
-        groupedData[account] = []
-    }
+    $scope.updateData = function() {
+        $scope.series = []
+        $scope.data = []
+        $scope.labels = []
 
-    for (var i = 0; i < data.length; i++) {
-        if (data[i].analysis) {
-            $scope.labels.push(data[i].text)
-            var angry = data[i].analysis.document_tone.tone_categories[0].tones[0].score
-            for (var accountId in groupedData) {
-                if (data[i].from.id == accountId) {
-                    groupedData[accountId].push(angry)
-                } else {
-                    groupedData[accountId].push(0)
+        var groupedData = {}
+        var accounts = {}
+        for (var i = 0; i < data.length; i++) {
+            if (!accounts[data[i].from.id]) {
+                var name = data[i].from.name || data[i].from.id
+                accounts[data[i].from.id] = name
+            } else {
+                if(accounts[data[i].from.id] == data[i].from.id && data[i].from.name) {
+                    accounts[data[i].from.id] = data[i].from.name
                 }
             }
         }
-    }
-    for (var accountName in groupedData) {
-        $scope.data.push(groupedData[accountName])
-    }
-
-    /**
-     * Chart
-     *
-     */
-    $scope.options = {
-        title: {
-            display: true,
-            text: 'Tone analysis for the conversation [Anger]'
-        },
-        legend: {
-            display: true,
-            position: 'bottom'
-        },
-        scales: {
-            xAxes: [{
-                display: false,
-                scaleLabel: {
-                    display: false
-                }
-            }],
-            yAxes: [{
-                ticks: {
-                    max: 1
-                }
-            }]
+        for (var account in accounts) {
+            $scope.series.push(accounts[account])
+            groupedData[account] = []
         }
-    };
-    $scope.onClick = function (points, evt) {
-        console.log(points, evt);
-    };
-    // $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+
+        for (var i = 0; i < data.length; i++) {
+            if (data[i].analysis) {
+                $scope.labels.push(data[i].text)
+                var angry = data[i].analysis.document_tone.tone_categories[0].tones[$scope.emotionToneId].score
+                for (var accountId in groupedData) {
+                    if (data[i].from.id == accountId) {
+                        groupedData[accountId].push(angry)
+                    } else {
+                        groupedData[accountId].push(0)
+                    }
+                }
+            }
+        }
+        for (var accountName in groupedData) {
+            $scope.data.push(groupedData[accountName])
+        }
+        /**
+         * Chart
+         *
+         */
+        $scope.options = {
+            title: {
+                display: true,
+                text: 'Tone analysis for the conversation [' + $scope.emotionTones[$scope.emotionToneId] +']'
+            },
+            legend: {
+                display: true,
+                position: 'bottom'
+            },
+            scales: {
+                xAxes: [{
+                    display: false,
+                    scaleLabel: {
+                        display: false
+                    }
+                }],
+                yAxes: [{
+                    ticks: {
+                        max: 1
+                    }
+                }]
+            }
+        };
+    }
+    $scope.updateData()
 }
 
 conversationController.resolve = {
